173fa99a5b31be98ad2d99486d8aefd377764b45
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2020-11-26 20:13:41 +0100

    new version

--- CRISPRidentify.py
+++ CRISPRidentify.py
@@ -7,10 +7,11 @@ from joblib import dump, load
 from time import time
 import multiprocessing
 import subprocess
-
+import warnings
+warnings.filterwarnings("ignore")
 sys.path.insert(0, 'components/')
-from predictor import Predictor
-from machine_learning import ClassifierWrapper
+from pipeline import Pipeline
+from components_ml import ClassifierWrapper
 import shutil
 from time import time
 import math
@@ -23,8 +24,8 @@ parser.add_argument('--input_folder', nargs='*', type=str, default=None,
 parser.add_argument('--file', type=str, default=None,
                     help='input file (default: None)')
 
-parser.add_argument('--model', type=str, default="8",
-                    help='model_to_use (default: 8)')
+parser.add_argument('--model', type=str, default="ALL",
+                    help='model_to_use (default: ALL)')
 
 parser.add_argument('--additional_model', type=str, default=None,
                     help='model_to_use (default: None)')
@@ -32,14 +33,14 @@ parser.add_argument('--additional_model', type=str, default=None,
 parser.add_argument('--result_folder', type=str, default="Results",
                     help='folder with the result (default: Results)')
 
-parser.add_argument('--pickle_report', type=str, default=None,
+parser.add_argument('--pickle_report', type=str, default='',
                     help='pickled report file (default: None)')
 
-parser.add_argument('--cas', type=bool, default=False,
-                    help='cas genes computation (default: False)')
+parser.add_argument('--cas', type=bool, default=True,
+                    help='cas genes computation (default: True)')
 
-parser.add_argument('--is_element', type=bool, default=False,
-                    help='is element computation (default: False)')
+parser.add_argument('--is_element', type=bool, default=True,
+                    help='is element computation (default: True)')
 
 parser.add_argument('--parallel', type=str, default=True,
                     help='parallel computations (default: True)')
@@ -68,9 +69,6 @@ parser.add_argument('--max_len_spacer', type=int, default=78,
 parser.add_argument('--min_repeats', type=int, default=3,
                     help='min number of repeats (default: 3)')
 
-parser.add_argument('--log_file', type=str, default=None,
-                    help='log file (default: None)')
-
 parser.add_argument('--enhancement_max_min', type=bool, default=True,
                     help='enhancement with filter (default: True)')
 
@@ -82,6 +80,12 @@ parser.add_argument('--max_identical_spacers', type=int, default=4,
 
 parser.add_argument('--max_identical_cluster_spacers', type=int, default=3,
                     help='maximum number of consecutive identical spacers in the array (default: 3)')
+
+parser.add_argument('--margin_degenerated', type=int, default=30,
+                    help='maximum length of the spacer margin for the degenerated search (default: 30)', )
+
+parser.add_argument('--max_edit_distance_enhanced', type=int, default=6,
+                    help='maximum edit distance for the evaluated array enhancement (default: 6)')
 
 
 args = parser.parse_args()
@@ -98,18 +102,13 @@ if complete_path_folder_info:
 
 complete_path_file = args.file
 folder_result = args.result_folder
-report_pickle = args.pickle_report
+pickle_folder = args.pickle_report
 list_models = ["8", "9", "10"] if args.model == "ALL" else [args.model]
 flag_possible_differentiate_model = args.additional_model
 if flag_possible_differentiate_model not in ["possible", "all"]:
     flag_possible_differentiate_model = None
 
-#flag_parallel = args.parallel
-#flag_cpu = args.cpu
-#cas_flag = args.cas
-#is_flag = args.is_element
-#degenerated_flag = args.degenerated
-#flag_fast_run = args.fast_run
+
 flag_enhancement_max_min = args.enhancement_max_min
 flag_enhancement_start_end = args.enhancement_start_end
 
@@ -121,6 +120,16 @@ cas_flag = False if (args.cas in ["False", False]) else True
 is_flag = False if (args.is_element in ["False", False]) else True
 degenerated_flag = False if (args.degenerated in ["False", False]) else True
 
+flags = {"flag_parallel": flag_parallel,
+         "flag_cpu": flag_cpu,
+         "flag_fast_run": flag_fast_run,
+         "flag_cas": cas_flag,
+         "flag_is": is_flag,
+         "flag_degenerated": degenerated_flag,
+         "flag_enhancement_min_max": flag_enhancement_max_min,
+         "flag_enhancement_start_end": flag_enhancement_start_end
+}
+
 min_rep = args.min_len_rep
 max_rep = args.max_len_rep
 max_spacer = args.max_len_spacer
@@ -128,6 +137,8 @@ min_spacer = args.min_len_spacer
 min_repeats = args.min_repeats
 max_identical_spacers = args.max_identical_spacers
 max_identical_cluster_spacers = args.max_identical_cluster_spacers
+margin_degenerated = args.margin_degenerated
+max_edit_distance_enhancement = args.max_edit_distance_enhanced
 
 parameters = {
     "param_min_avg_repeat_length": min_rep,
@@ -136,10 +147,10 @@ parameters = {
     "param_min_avg_spacer_length": min_spacer,
     "param_min_repeats": min_repeats,
     "param_max_identical_spacers": max_identical_spacers,
-    "param_max_identical_cluster_spacers": max_identical_cluster_spacers
+    "param_max_identical_cluster_spacers": max_identical_cluster_spacers,
+    "param_spacer_margin_degenerated_search": margin_degenerated,
+    "param_max_edit_distance": max_edit_distance_enhancement
 }
-
-log_file = args.log_file
 
 
 ALL_FEATURES = ['repeat_len', 'number_repeats', 'repeat_similarity',
@@ -163,8 +174,7 @@ list_ml_classifiers = [ClassifierWrapper(classifier_type=None,
 possible_differentiate_model = load('training_new_model_possible_split/random_forest_positive.joblib')
 
 
-def run_over_folder_of_files(folder, result_folder, chunk_number=None, number_of_chunks=None,
-                             report_pickle_folder=report_pickle):
+def run_over_folder_of_files(folder, result_folder, pickle_folder, chunk_number=None, number_of_chunks=None):
 
     files = [f for f in listdir(folder) if isfile(join(folder, f))]
     files = sorted(files)
@@ -181,45 +191,28 @@ def run_over_folder_of_files(folder, result_folder, chunk_number=None, number_of
 
     for index, file in enumerate(chunk, 1):
         print("\n\n\n\t\t\t\tExecuting file {} out of {} ({})\n\n\n".format(index, len(chunk), file))
-        pr = Predictor(result_folder_path="{}/".format(result_folder),
-                       file_path=join(folder, file),
-                       eden_classifier=None, list_ml_classifiers=list_ml_classifiers,
-                       possible_differentiate_model=possible_differentiate_model,
-                       flag_possible_differential_model=flag_possible_differentiate_model,
-                       list_features=feature_list, flag_is=is_flag, flag_cas=cas_flag,
-                       flag_degenerated=degenerated_flag,
-                       flag_parallel=flag_parallel,
-                       flag_cpu=flag_cpu,
-                       flag_fast_run=flag_fast_run,
-                       flag_enhancement_max_min=flag_enhancement_max_min,
-                       flag_enhancement_start_end=flag_enhancement_start_end,
-                       parameters=parameters,
-                       log_file=log_file
-                       )
-
-        if report_pickle_folder:
-            pr.report_into_separate_file(report_pickle_folder)
+        pl = Pipeline(result_folder_path="{}/".format(result_folder),
+                      pickle_folder_path="{}".format(pickle_folder),
+                      file_path=join(folder, file),
+                      list_ml_classifiers=list_ml_classifiers,
+                      possible_differentiate_model=possible_differentiate_model,
+                      flag_possible_differential_model=flag_possible_differentiate_model,
+                      list_features=feature_list,
+                      parameters=parameters,
+                      flags=flags)
 
 
-def run_over_one_file(file, result_folder, report_pickle_folder=report_pickle):
+def run_over_one_file(file, result_folder, pickle_folder):
     print("\n\n\n\t\t\t\tExecuting file {}\n\n\n".format(file))
-    pr = Predictor(result_folder_path="{}/".format(result_folder),
-                   file_path="{}".format(file),
-                   eden_classifier=None, list_ml_classifiers=list_ml_classifiers,
-                   possible_differentiate_model=possible_differentiate_model,
-                   flag_possible_differential_model=flag_possible_differentiate_model,
-                   list_features=feature_list, flag_is=is_flag, flag_cas=cas_flag,
-                   flag_degenerated=degenerated_flag,
-                   flag_parallel=flag_parallel,
-                   flag_cpu=flag_cpu,
-                   flag_fast_run=flag_fast_run,
-                   parameters=parameters,
-                   flag_enhancement_max_min=flag_enhancement_max_min,
-                   flag_enhancement_start_end=flag_enhancement_start_end,
-                   log_file=log_file)
-
-    if report_pickle_folder:
-        pr.report_into_separate_file(report_pickle_folder)
+    pl = Pipeline(result_folder_path="{}/".format(result_folder),
+                  pickle_folder_path="{}".format(pickle_folder),
+                  file_path=join(file),
+                  list_ml_classifiers=list_ml_classifiers,
+                  possible_differentiate_model=possible_differentiate_model,
+                  flag_possible_differential_model=flag_possible_differentiate_model,
+                  list_features=feature_list,
+                  parameters=parameters,
+                  flags=flags)
 
 
 def multiline_fasta_check(file):
@@ -255,12 +248,12 @@ if __name__ == "__main__":
             print("Multifasta")
             folder_multifasta = multiline_fasta_handle(complete_path_file)
             print(folder_multifasta)
-            run_over_folder_of_files(folder_multifasta, folder_result)
+            run_over_folder_of_files(folder_multifasta, folder_result, pickle_folder)
             shutil.rmtree(folder_multifasta)
         else:
-            run_over_one_file(complete_path_file, folder_result)
+            run_over_one_file(complete_path_file, folder_result, pickle_folder)
     else:
-        run_over_folder_of_files(complete_path_folder, folder_result,
+        run_over_folder_of_files(complete_path_folder, folder_result, pickle_folder,
                                  chunk_number=chunk_number, number_of_chunks=number_of_chunks)
 
     end_time = time()
--- components/log_file_operations.py
+++
@@ -1,3 +0,0 @@
-def logging(log_file, string):
-    with open(log_file, "a") as f:
-        f.write(string)
